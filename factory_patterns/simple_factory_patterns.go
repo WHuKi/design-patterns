f79e5d5fab64839518b99cef884a7cced98dc6d4
huxiaoke <huxiaoke515@163.com>
2021-11-11 14:02:01 +0800

    添加工厂模式

---
+++ factory_patterns/simple_factory_patterns.go
@@ -0,0 +1,36 @@
+package factory_patterns
+
+// 简单工厂模式
+// IRuleConfigParser IRuleConfigParser
+type IRuleConfigParser interface {
+	Parse(data []byte)
+}
+
+// jsonRuleConfigParser jsonRuleConfigParser
+type jsonRuleConfigParser struct {
+}
+
+// Parse Parse
+func (J jsonRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+// yamlRuleConfigParser yamlRuleConfigParser
+type yamlRuleConfigParser struct {
+}
+
+// Parse Parse
+func (Y yamlRuleConfigParser) Parse(data []byte) {
+	panic("implement me")
+}
+
+// NewIRuleConfigParser NewIRuleConfigParser
+func NewIRuleConfigParser(t string) IRuleConfigParser {
+	switch t {
+	case "json":
+		return jsonRuleConfigParser{}
+	case "yaml":
+		return yamlRuleConfigParser{}
+	}
+	return nil
+}
