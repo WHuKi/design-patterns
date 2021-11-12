8ed462d679b84f6b350b7cd1670fa1b4079e4a71
huxiaoke <huxiaoke515@163.com>
2021-11-12 13:25:57 +0800

    添加构建者模式

---
+++ create_type/builder_patterns/builder_patterns.go
@@ -0,0 +1,72 @@
+package builder_patterns
+
+import "fmt"
+
+// 建造者模式
+// 适用场景
+// 参数比较多
+// 有些参数必须传 有些可不传
+
+const (
+	defaultMaxTotal = 10
+	defaultMaxIdle  = 9
+	defaultMinIdle  = 1
+)
+
+// ResourcePoolConfig resource pool
+type ResourcePoolConfig struct {
+	name     string
+	maxTotal int
+	maxIdle  int
+	minIdle  int
+}
+
+// ResourcePoolConfigBuilder 用于构建 ResourcePoolConfig
+type ResourcePoolConfigBuilder struct {
+	name     string
+	maxTotal int
+	maxIdle  int
+	minIdle  int
+}
+
+// ResourcePoolConfigOption option
+type ResourcePoolConfigOption struct {
+	maxTotal int
+	maxIdle  int
+	minIdle  int
+}
+
+// ResourcePoolConfigOptFunc to set option
+type ResourcePoolConfigOptFunc func(option *ResourcePoolConfigOption)
+
+// NewResourcePoolConfig NewResourcePoolConfig
+func NewResourcePoolConfig(name string, opts ...ResourcePoolConfigOptFunc) (*ResourcePoolConfig, error) {
+	if name == "" {
+		return nil, fmt.Errorf("name can not be empty")
+	}
+
+	option := &ResourcePoolConfigOption{
+		maxTotal: 10,
+		maxIdle:  9,
+		minIdle:  1,
+	}
+
+	for _, opt := range opts {
+		opt(option)
+	}
+
+	if option.maxTotal < 0 || option.maxIdle < 0 || option.minIdle < 0 {
+		return nil, fmt.Errorf("args err, option: %v", option)
+	}
+
+	if option.maxTotal < option.maxIdle || option.minIdle > option.maxIdle {
+		return nil, fmt.Errorf("args err, option: %v", option)
+	}
+
+	return &ResourcePoolConfig{
+		name:     name,
+		maxTotal: option.maxTotal,
+		maxIdle:  option.maxIdle,
+		minIdle:  option.minIdle,
+	}, nil
+}
