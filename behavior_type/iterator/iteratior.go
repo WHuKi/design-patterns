2ec9a8f301f0d7007d8c2aafbaf907b1300fe323
huxiaoke <huxiaoke515@163.com>
2021-11-25 14:12:08 +0800

    添加迭代器模式

---
+++ behavior_type/iterator/iteratior.go
@@ -0,0 +1,48 @@
+package iterator
+
+//
+type iterator interface {
+	hasNext() bool
+	getNext() *user
+}
+
+type collection interface {
+	createIterator() iterator
+}
+
+type user struct {
+	name string
+	age  int
+}
+
+// 迭代器实现
+type userIterator struct {
+	index int
+	users []*user
+}
+
+func (u *userIterator) hasNext() bool {
+	if u.index < len(u.users) {
+		return true
+	}
+	return false
+}
+
+func (u *userIterator) getNext() *user {
+	if u.hasNext() {
+		user := u.users[u.index]
+		u.index++
+		return user
+	}
+	return nil
+}
+
+type userCollection struct {
+	users []*user
+}
+
+func (u *userCollection) createIterator() iterator {
+	return &userIterator{
+		users: u.users,
+	}
+}
